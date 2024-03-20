5147cab7e54a9268a84f09675c0ffe9f314ab824
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-03-21 00:35:39 +0330

    feature/ initialize transformation form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,12 +1,16 @@
 import Header from "@/components/shared/Header";
 import React from "react";
 import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
 
 const AddTransformationType = ({ params: { type } }: SearchParamProps) => {
   const transforamtion = transformationTypes[type];
 
   return (
-    <Header title={transforamtion.title} subtitle={transforamtion.subTitle} />
+    <>
+      <Header title={transforamtion.title} subtitle={transforamtion.subTitle} />
+      <TransformationForm />
+    </>
   );
 };
 
